affa6f3fabc40cce019ddcab8460c3d125251af6
Jan Kalinič <xkalin13@vutbr.cz>
2021-03-10 21:30:22 +0100

    added cv5

---
+++ cv5/cv5/Auto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cv5
+{
+    class Auto
+    {
+        public enum TypPaliva
+        {
+            BENZIN,
+            NAFTA
+        }
+
+        //protected kvuli odvozenym tridam
+        public double VelikostNadrze { get; protected set; }
+        public double StavNadrze { get; protected set; }
+        public TypPaliva Palivo { get; protected set; }
+
+        private AutoRadio Radio = new AutoRadio();
+
+        public Auto(TypPaliva palivo, double stavNadrze) {//nemuze mit velikost nadrze-je specificka
+            this.Palivo = palivo;
+            this.StavNadrze = stavNadrze;        
+        }
+        public void Natankuj(TypPaliva tankovanePalivo, double mnozstvi) {
+
+            if (tankovanePalivo != this.Palivo ) {//enum
+                throw new Exception("spatne palivo");
+            }
+            else {
+                if (StavNadrze + mnozstvi < VelikostNadrze)
+                {
+                    StavNadrze += mnozstvi;
+                }
+                else {
+                    throw new Exception("jejda nadrz pretekla, to bude penez");
+                }
+                
+            }
+            
+        }
+        public void ZapnoutRadio(bool radioZapnuto)
+        {
+            Radio.ZapnoutRadio(radioZapnuto);
+        }
+        public void NastavPredvolbu(int cislo, double kmitocet)
+        {
+            Radio.NastavPredvolbu(cislo, kmitocet);
+        }
+        public void PreladNaPredvolbu(int cislo)
+        {
+            Radio.PreladNaPredvolbu(cislo);
+        }
+        public void RadioToString()
+        {
+            Console.WriteLine(Radio);
+        }
+
+    }
+}
---
+++ cv5/cv5/Nakladni.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cv5
+{
+    class Nakladni : Auto
+    {
+        public int MaxNaklad { get; private set; }
+        private int PrepravovanyNaklad{ get; set; }
+
+        public Nakladni(TypPaliva palivo, double stavNadrze, int prepravovanyNaklad = 0) : base(palivo, stavNadrze){
+            //base vychozi-neco jako super v jave
+
+           
+            this.MaxNaklad = 200; //to by se nemelo menit, nastavim jej zde
+            this.VelikostNadrze = 400; //to by se taky nemelo menit
+
+            if (this.MaxNaklad >= prepravovanyNaklad)//osetreni vyssiho zadani
+            {
+                this.PrepravovanyNaklad = prepravovanyNaklad;
+            }
+            else {
+                throw new Exception("Do nakladaku se naklad " + MaxNaklad + "  nikoliv " + PrepravovanyNaklad);
+            }
+            
+
+        }
+        public override string ToString()
+        {
+            return "MaxNaklad: " + MaxNaklad + " a prepravovany: " + PrepravovanyNaklad;
+        }
+    }
+}
